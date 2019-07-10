8e2e97109ff3d012bc89860a7a36267589164ca0
rikutech <rikutech@gmail.com>
2019-07-11 02:18:30 +0900

    tmp

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import cv2
 import numpy as np
+import itertools
+
+def resize_img(img):
+    width = 500
+    scale = width / img.shape[1]
+    return cv2.resize(img, dsize=None, fx=scale, fy=scale)
+
+def childCount(hierarchy, index):
+    target = hierarchy[index]
+    childIndex = target[2]
+    #next(itertools.ifilter(lambda x:len(x) >= 4, hierarchy), None)
 
 origin_img = cv2.imread('./input.jpg')
-lined_img  = cv2.imread('./input.jpg')
+origin_img = resize_img(origin_img)
+width = origin_img.shape[1]
+height = origin_img.shape[0]
+
 target_img = cv2.imread('./input.jpg', 0)
-target_img = cv2.Canny(target_img, 700, 1000)
-contours, hierarchy = cv2.findContours(target_img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-hierarchy = hierarchy[0]
-ls = list(filter(lambda n:len(n[0]) > 500, zip(contours, hierarchy)))
-contours = list(map(lambda n:n[0], ls))
-hierarchy = list(map(lambda n:n[1], ls))
+target_img = resize_img(target_img)
 
-for i in range(len(contours)):
-    cv2.drawContours(target_img, contours, i, (255, 255, 255), thickness=5)
+blank_img = np.zeros((height, width, 3), np.uint8)
 
+target_img = cv2.Canny(target_img, 200, 500)
 cv2.imwrite('./target.jpg', target_img)
-contours, hierarchy = cv2.findContours(target_img, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+contours, hierarchy = cv2.findContours(target_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 hierarchy = hierarchy[0]
-ls = list(filter(lambda n:len(n[0]) > 300, zip(contours, hierarchy)))
+ls = list(filter(lambda n:len(n[0]) > 100, zip(contours, hierarchy)))
+contours = list(map(lambda n:n[0], ls))
+hierarchy = list(map(lambda n:n[1], ls))
+
+
+for i in range(len(contours)):
+    if hierarchy[i][3] <= 0 and cv2.contourArea(contours[i]) > 10000:
+        cv2.drawContours(blank_img, contours, i, (255, 255, 255), thickness=2)
+cv2.imwrite('./blank.jpg', blank_img)
+blank_img = cv2.cvtColor(blank_img, cv2.COLOR_RGB2GRAY)
+contours, hierarchy = cv2.findContours(blank_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+hierarchy = hierarchy[0]
+ls = list(filter(lambda n:len(n[0]) > 100, zip(contours, hierarchy)))
 contours = list(map(lambda n:n[0], ls))
 hierarchy = list(map(lambda n:n[1], ls))
 
 for i in range(len(contours)):
-    if hierarchy[i][2] > -1:
+    if cv2.contourArea(contours[i]) > 10000:
         cv2.drawContours(origin_img, contours, i, (255, 255, 255), thickness=-1)
 
         moment = cv2.moments(contours[i])
